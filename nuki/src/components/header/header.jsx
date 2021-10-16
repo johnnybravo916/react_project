e76af96d0335729188e4da2722f6221ccb089d77
John Ong <john.ong@shopify.com>
2021-10-16 17:54:46 -0500

    updated layout html and created a getData function

--- nuki/src/components/header/header.jsx
+++ nuki/src/components/header/header.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-function Header() {
-    return <h1>Header</h1>;
+import Navigation from "../navigation/navigation";
+
+export default function Header() {
+    const [data, setData] = useState([]);
+
+    function getData() {
+        fetch("./data/data.json")
+            .then((response) => {
+                if (response.status !== 200) {
+                    console.log(response.status);
+                }
+                return response.json();
+            })
+            .then((response) => {
+                console.log(response);
+                setData(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <header>
+            {Object.entries(data).map(([key, value]) => {
+                return (
+                    <>
+                        {value.map((d) => (
+                            <a href="#" title={d.title}>
+                                {d.title}
+                            </a>
+                        ))}
+                    </>
+                );
+            })}
+            <Navigation />
+        </header>
+    );
 }
-
-export default Header;
