d74127ecec41ed702656eed032f2e3a9baec8f4f
johnnybravo916 <johnong916@gmail.com>
2021-10-17 09:21:53 -0500

    updated fetch

--- nuki/src/components/header/header.jsx
+++ nuki/src/components/header/header.jsx
@@ -1,44 +1,12 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 
 import Navigation from "../navigation/navigation";
 
-export default function Header() {
-    const [data, setData] = useState([]);
-
-    function getData() {
-        fetch("./data/data.json")
-            .then((response) => {
-                if (response.status !== 200) {
-                    console.log(response.status);
-                }
-                return response.json();
-            })
-            .then((response) => {
-                console.log(response);
-                setData(response);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-
-    useEffect(() => {
-        getData();
-    }, []);
+export default function Header({props}) {
 
     return (
         <header>
-            {Object.entries(data).map(([key, value]) => {
-                return (
-                    <>
-                        {value.map((d) => (
-                            <a href="#" title={d.title}>
-                                {d.title}
-                            </a>
-                        ))}
-                    </>
-                );
-            })}
+            <div>{props.description}</div>
             <Navigation />
         </header>
     );
