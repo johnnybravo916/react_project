e95dec25b2ca31ea3a57ddf6274879be6cf6720b
John Ong <john.ong@shopify.com>
2021-10-21 15:07:33 -0500

    updated contact and delivery section

--- nuki/src/components/contact/contact.jsx
+++ nuki/src/components/contact/contact.jsx
@@ -2,9 +2,31 @@ import React from "react";
 
 export default function Contact(props) {
     return (
-        <section className="contact">
-            <h2>Get in Touch</h2>
-            <p>{props.data.address}</p>
+        <section
+            id="contact"
+            className="border-top border-bottom center-content"
+        >
+            <div className="contact">
+                <h2>{props.data.title}</h2>
+                <h3>{props.data.subheader}</h3>
+                <ul>
+                    {Object.entries(props.data.hours).map(([v, i]) => {
+                        return (
+                            <li>
+                                {i.day} - {i.time}
+                            </li>
+                        );
+                    })}
+                </ul>
+                <a
+                    href={props.data.address_link}
+                    title={props.data.address}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <address>{props.data.address}</address>
+                </a>
+            </div>
         </section>
     );
 }
