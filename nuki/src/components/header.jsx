0ea71c2f62ee6c45dd0ffeb44b09bfbff796ddf4
John Ong <john.ong@shopify.com>
2021-10-23 13:35:28 -0500

    updated branding

--- nuki/src/components/header.jsx
+++ nuki/src/components/header.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import logo from "../assets/icons/branding-large.png";
 
 import Navigation from "./navigation";
 
 export default function Header(props) {
     return (
         <header>
-            <a href="/" title={props.data.title}>
+            <a href="/" className="branding" title={props.data.title}>
+                <img
+                    src={logo}
+                    alt={props.data.title}
+                    title={props.data.title}
+                />
                 {props.data.title}
             </a>
             <Navigation title={props.data.title} />
