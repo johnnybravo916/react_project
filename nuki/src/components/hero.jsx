dd59b97e2ecbb9c9167611b94b1d7d3513042c3d
John Ong <john.ong@shopify.com>
2021-11-15 14:54:40 -0600

    updated images in hero section

--- nuki/src/components/hero.jsx
+++ nuki/src/components/hero.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import background from "../assets/images/background.jpg";
 
 export default function Hero(props) {
+    let images = [1, 2, 3];
+    images = images.sort(() => Math.random() - 0.5);
+
     return (
         <section id="hero" className="hero">
             <div className="container">
                 <h1>{props.data.header}</h1>
                 <h2>{props.data.subheader}</h2>
             </div>
-            <img src={background} alt={props.data.header} />
+            <img
+                src={`./images/background-${images[0]}.jpg`}
+                alt={props.data.header}
+            />
         </section>
     );
 }
