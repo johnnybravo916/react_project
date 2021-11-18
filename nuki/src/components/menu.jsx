a0000cbf72b5308d39b605192265d09b2d0cfe1e
John Ong <john.ong@shopify.com>
2021-11-18 13:54:51 -0600

    implemented tabs for menu

--- nuki/src/components/menu.jsx
+++ nuki/src/components/menu.jsx
@@ -1,29 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Menu(props) {
+    const [currentTab, setCurrentTab] = useState("Starters");
+
     return (
         <section>
             <div id="menu" className="menu">
                 {Object.entries(props.data).map(([key, value]) => {
                     return (
-                        <div className="menu__block">
-                            <h3>{value.title}</h3>
-                            <ul>
-                                {Object.values(value.content).map((v, i) => {
-                                    return (
-                                        <li>
-                                            <strong>{v.title}</strong>
-                                            <span className="description">
-                                                {v.description}
-                                            </span>
-                                            <span className="price">
-                                                ${v.price}
-                                            </span>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        </div>
+                        <>
+                            <div className="menu__block">
+                                <div className="tabs">
+                                    <button
+                                        onClick={() =>
+                                            setCurrentTab(value.title)
+                                        }
+                                    >
+                                        <h3
+                                            className={
+                                                value.title === currentTab
+                                                    ? "test"
+                                                    : null
+                                            }
+                                        >
+                                            {value.title}
+                                        </h3>
+                                    </button>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="tab-content">
+                                    <ul>
+                                        {Object.values(value.content).map(
+                                            (v, i) => {
+                                                if (
+                                                    value.title === currentTab
+                                                ) {
+                                                    return (
+                                                        <li>
+                                                            <strong>
+                                                                {v.title}
+                                                            </strong>
+                                                            <span className="description">
+                                                                {v.description}
+                                                            </span>
+                                                            <span className="price">
+                                                                ${v.price}
+                                                            </span>
+                                                        </li>
+                                                    );
+                                                } else {
+                                                    return null;
+                                                }
+                                            }
+                                        )}
+                                    </ul>
+                                </div>
+                            </div>
+                        </>
                     );
                 })}
             </div>
