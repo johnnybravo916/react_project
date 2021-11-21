ee5ffccb3895a6ae64401e54e68640ad6953cce4
John Ong <john.ong@shopify.com>
2021-11-21 12:02:38 -0600

    updated menu styling

--- nuki/src/components/menu.jsx
+++ nuki/src/components/menu.jsx
@@ -6,60 +6,51 @@ export default function Menu(props) {
     return (
         <section>
             <div id="menu" className="menu">
-                {Object.entries(props.data).map(([key, value]) => {
-                    return (
-                        <>
-                            <div className="menu__block">
-                                <div className="tabs">
-                                    <button
-                                        onClick={() =>
-                                            setCurrentTab(value.title)
+                <div className="menu__nav">
+                    {Object.entries(props.data).map(([key, value]) => {
+                        return (
+                            <button
+                                onClick={() => setCurrentTab(value.title)}
+                                className={
+                                    value.title === currentTab ? "active" : null
+                                }
+                            >
+                                <h3>{value.title}</h3>
+                            </button>
+                        );
+                    })}
+                </div>
+                <div className="menu__content">
+                    {Object.entries(props.data).map(([key, value]) => {
+                        return (
+                            <div className="tab-content">
+                                <ul>
+                                    {Object.values(value.content).map(
+                                        (v, i) => {
+                                            if (value.title === currentTab) {
+                                                return (
+                                                    <li>
+                                                        <strong>
+                                                            {v.title}
+                                                        </strong>
+                                                        <span className="description">
+                                                            {v.description}
+                                                        </span>
+                                                        <span className="price">
+                                                            ${v.price}
+                                                        </span>
+                                                    </li>
+                                                );
+                                            } else {
+                                                return null;
+                                            }
                                         }
-                                    >
-                                        <h3
-                                            className={
-                                                value.title === currentTab
-                                                    ? "test"
-                                                    : null
-                                            }
-                                        >
-                                            {value.title}
-                                        </h3>
-                                    </button>
-                                </div>
+                                    )}
+                                </ul>
                             </div>
-                            <div>
-                                <div className="tab-content">
-                                    <ul>
-                                        {Object.values(value.content).map(
-                                            (v, i) => {
-                                                if (
-                                                    value.title === currentTab
-                                                ) {
-                                                    return (
-                                                        <li>
-                                                            <strong>
-                                                                {v.title}
-                                                            </strong>
-                                                            <span className="description">
-                                                                {v.description}
-                                                            </span>
-                                                            <span className="price">
-                                                                ${v.price}
-                                                            </span>
-                                                        </li>
-                                                    );
-                                                } else {
-                                                    return null;
-                                                }
-                                            }
-                                        )}
-                                    </ul>
-                                </div>
-                            </div>
-                        </>
-                    );
-                })}
+                        );
+                    })}
+                </div>
             </div>
         </section>
     );
